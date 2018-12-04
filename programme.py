8eb2ce3b20f143b32a121819a1b920527579f82a
Louis <louismaury33@gmail.com>
2018-12-04 17:45:48 +0100

    Amelioration du programme

--- programme.py
+++ programme.py
@@ -2,13 +2,27 @@ from tkinter import *
 
 # On crée une fenêtre
 fenetre = Tk()
-photo = PhotoImage(file = "balle.png")
+
+largeur = 500
+hauteur = 500
 # On dit de faire une toile de 500px de largeur et de 500px de hauteur
-canvas = Canvas(fenetre , width = 500 , height = 500)
-canvas.create_image(250 , 250 , anchor = NW , image = photo)
+canvas = Canvas(fenetre , width = largeur , height = hauteur)
 canvas.pack()
+
+
+def spawn():
+    photo = PhotoImage(file = "balle.png")
+    canvas.create_image(largeur/2-25 , hauteur/2-25 , anchor = NW , image = photo)
+    canvas.pack()
+
+    
+label = Label(fenetre , text = "PONG !")
+bouton_depart = Button(fenetre , text = "C'est partie !" , command = spawn())
+label.pack()
+bouton_depart.pack()
 
 
 # On demande au programme d'afficher la fenêtre
 canvas.pack()
+
 fenetre.mainloop()
