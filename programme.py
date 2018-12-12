28d233bc225e27ea187dcdfef1d116563283de91
Louis <louismaury33@gmail.com>
2018-12-12 13:36:10 +0100

    Creation d'une "balle"
    Et on fait en sorte qu'elle rebondisse

--- programme.py
+++ programme.py
@@ -1,28 +1,67 @@
 from tkinter import *
+import random
+import time
 
 # On crée une fenêtre
 fenetre = Tk()
+# Le titre de la fenetre affichera "Pong"
+fenetre.title("Pong")
 
-largeur = 500
+largeur = 1000
 hauteur = 500
+
 # On dit de faire une toile de 500px de largeur et de 500px de hauteur
-canvas = Canvas(fenetre , width = largeur , height = hauteur)
+canvas = Canvas(fenetre , width = largeur , height = hauteur , bd = 0 , highlightthickness = 0 , bg = "black")
 canvas.pack()
+fenetre.update()
 
+start = [-1 , 1]
+random.shuffle(start)
 
-def spawn():
-    photo = PhotoImage(file = "balle.png")
-    canvas.create_image(largeur/2-25 , hauteur/2-25 , anchor = NW , image = photo)
-    canvas.pack()
+class Ball:
+
+    def __init__(self , canvas , color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(largeur/2-10 , hauteur/2-10 , largeur/2+10 , hauteur/2+10 , fill = color)
+        self.canvas.move(self.id , 2 , 2)
+        self.x = start[0]
+        self.y = start[1]
+        self.hauteur_canevas = self.canvas.winfo_height()
+        self.largeur_canevas = self.canvas.winfo_width()
 
     
-label = Label(fenetre , text = "PONG !")
-bouton_depart = Button(fenetre , text = "C'est partie !" , command = spawn())
-label.pack()
-bouton_depart.pack()
+    def dessiner(self):
+        self.canvas.move(self.id , self.x , self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.hauteur_canevas:
+            self.y = -1
+        
+        if pos[0] <= 0:
+            self.x = 1
+        if pos[2] >= self.largeur_canevas:
+            self.x = -1
+
+class Raquette:
+    def __init__(self , canvas , color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(5, 5 , 10 , 100 , fill = color)
+        self.canvas.move(self.id , 200 , 300)
+    
+    def dessiner(self):
+        pass
 
 
-# On demande au programme d'afficher la fenêtre
-canvas.pack()
+raq = Raquette(canvas , "white")
+balle = Ball(canvas , "white")
+
+while True:
+    balle.dessiner()
+    raq.dessiner()
+    fenetre.update_idletasks()
+    fenetre.update()
+    time.sleep(0.01)
+
 
 fenetre.mainloop()
--- programme/programme.py
+++
@@ -1,4 +0,0 @@
-from tkinter import *
-tk = Tk()
-btn = Button(tk , text = "Clique sur moi")
-btn.pack()
